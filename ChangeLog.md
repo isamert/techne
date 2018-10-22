8ca70a889ec9407252cbb4801b5877388cd514e0
İsa Mert Gürbüz <isamertgurbuz@gmail.com>
2018-10-23 01:35:22 +0300

    add basic cmdline option parsing and a repl template

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for techne
+
+## Unreleased changes
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,59 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
-import BasicPrelude
-import Lib
+import TechnePrelude
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+import System.Console.Haskeline
+
+--
+-- Commandline options
+--
+data Options = Options
+  { interactive :: Bool
+  , output :: Text }
+
+optInteractive = switch $
+    long "interactive"
+    <> short 'i'
+    <> help "Start an interactive Techne shell."
+
+optOutput = strOption $
+    long "output"
+    <> metavar "<file>"
+    <> help "Place the output into <file>"
+
+options :: Parser Options
+options = Options
+    <$> optInteractive
+    <*> optOutput
+
+--
+-- Main
+--
+
+-- | Get commandline options and display information if necessary.
 main :: IO ()
-main = someFunc
+main = runOptions =<< execParser opts
+  where
+    opts = info (options <**> helper)
+      ( fullDesc
+     <> progDesc "Compile files or run an interactive shell."
+     <> header "Techne" )
+
+-- | Do stuff depending on commandline options.
+runOptions :: Options -> IO ()
+runOptions (Options True _) = runInputT defaultSettings repl
+runOptions _ = return ()
+
+
+repl :: InputT IO ()
+repl = getInputLine "> " >>= \case
+    Nothing    -> repl
+    Just ":q"  -> return ()
+    Just input -> do { outputStrLn $ "Input was: " ++ input; repl }
+
+
+
